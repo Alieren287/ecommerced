7a72472ad851515092b3a215a5ced82fb0ce16ee
alieren <alierenyavuzaslan@gmail.com>
2025-07-13 20:27:38 +0300

    response fix

---
+++ ecommerced-web-core/src/test/java/com/alier/ecommercedwebcore/EcommercedWebCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alier.ecommercedwebcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommercedWebCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
